20e565e225b425bb2fe3d6d3d02ecd69d5e31b14
sjndowair <wodnjsdl1125@naver.com>
2025-04-23 13:15:29 +0900

    api 호출 키 추가

---
+++ app/apis/getUpbitMarketData.ts
@@ -0,0 +1,2 @@
+export const API_KEY = process.env.NEXT_APP_API_KEY
+export const BASE_URL = "https://api.openweathermap.org"
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,23 @@
-import Main from "./components/Main/index";
+"use client";
+import { useState } from "react";
+import {} from "lucide-react";
+
+
+type TperiodType = "1D" | "1W" | "1M" | "6M" | "1Y"
 
 export default function Home() {
-  return (
-    <div className="space-y-8">
-      <Main />
-    </div>
-  );
+  const [isOpenMarket, setIsOpenMarket] = useState({
+    krw: false,
+    btc: false,
+    usdt: false,
+    eth: false
+  });
+
+  const [chartType , setChartType] = useState<"line" | "bar" | "area">("line");
+  const [period, setPeriod ] = useState<TperiodType>("1Y");
+  const [selectedMarket, setSelectedMarket] = useState<string | null>(null);
+  const [searchTerm, setSearchTerm] = useState<string>("");
+
+
+  return (<div className="space-y-8"></div>);
 }
