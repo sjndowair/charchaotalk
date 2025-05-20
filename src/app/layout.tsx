976ba22501f198cc051cb2e2102f3e37bddf12a1
sjndowair <wodnjsdl1125@naver.com>
2025-05-20 14:38:12 +0900

    fix: components 폴더 위치 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Footer from "./components/footer";
-import Header from "./components/header";
+import Footer from "../components/footer";
+import Header from "../components/header";
 
 
 const geistSans = Geist({
